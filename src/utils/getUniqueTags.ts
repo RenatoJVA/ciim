0b27107c1d62b15c6dd67f04eedf8f64cb4f0ee8
RenatoUCSM <renato.vace@gmail.com>
2024-01-11 12:02:45 -0500

    jue 11 ene 2024 12:02:45 -05

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -1,14 +1,14 @@
 import { slugifyStr } from "./slugify";
 import type { CollectionEntry } from "astro:content";
 
-interface Tag {
+interface Actividades {
   tag: string;
   tagName: string;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
   const filteredPosts = posts.filter(({ data }) => !data.draft);
-  const tags: Tag[] = filteredPosts
+  const actividades: Actividades[] = filteredPosts
     .flatMap(post => post.data.tags)
     .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
     .filter(
@@ -16,7 +16,7 @@ const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
         self.findIndex(tag => tag.tag === value.tag) === index
     )
     .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
-  return tags;
+  return actividades;
 };
 
 export default getUniqueTags;
